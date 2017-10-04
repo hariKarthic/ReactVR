135ff687f4de22ef85acef3452858e4f832211cc
hKarthic <m.harikarthic@gmail.com>
2017-10-04 18:28:34 +0530

    Added custom raycaster

---
+++ raycaster.vr.js
@@ -0,0 +1,9 @@
+const SimpleRaycaster = {
+    getType: () => "simple",
+    getRayOrigin: () => [0, 0, 0],
+    getRayDirection: () => [0, 0, -1],
+    drawsCursor: () => true
+};
+
+
+module.exports = SimpleRaycaster;
